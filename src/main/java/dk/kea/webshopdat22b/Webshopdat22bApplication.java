0af73f69288ec7b1692cfc2f80a18c129c4f44eb
jarltuxen <jarl.tuxen@gmail.com>
2023-03-17 14:03:05 +0100

    Initial commit

---
+++ src/main/java/dk/kea/webshopdat22b/Webshopdat22bApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.webshopdat22b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webshopdat22bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Webshopdat22bApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/webshopdat22b/Webshopdat22bApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.webshopdat22b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webshopdat22bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
