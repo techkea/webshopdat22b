526e738ebcc2bd9d3b0ac1a4925b7c6fe38de985
jarltuxen <jarl.tuxen@gmail.com>
2023-03-17 14:47:04 +0100

    database create script

---
+++ src/main/resources/create-db.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS webshopdat22b;
+CREATE SCHEMA webshopdat22b ;
+DROP TABLE IF EXISTS products;
+CREATE TABLE webshopdat22b.products (
+                                            `id` INT NOT NULL AUTO_INCREMENT,
+                                            `name` VARCHAR(30) NOT NULL,
+                                            `price` DECIMAL(13,2) NULL,
+                                            PRIMARY KEY (`id`));
+INSERT INTO `webshopdat22b`.`products` (`name`, `price`) VALUES ('Tuborg', '25');
+INSERT INTO `webshopdat22b`.`products` (`name`, `price`) VALUES ('Carlsberg', '25');
+
